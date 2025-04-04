9a9965db3b8e262753904b6a5868f26a09416865
Jatin Sandilya <sandilya.jatin@gmail.com>
2025-04-04 16:56:41 +0530

    feat: handle busy port

--- src/connect.ts
+++ src/connect.ts
@@ -1,10 +1,13 @@
 import { Server } from '@modelcontextprotocol/sdk/server/index.js';
 import { SSEServerTransport } from '@modelcontextprotocol/sdk/server/sse.js';
 import express, { Request, Response } from 'express';
+import { detect } from 'detect-port';
 
+const DEFAULT_PORT = 3001;
 export async function connectServer(server: Server): Promise<express.Application> {
     const app = express();
-    const port = 3001;
+    const port = await detect(DEFAULT_PORT);
+
     let currentTransport: SSEServerTransport | null = null;
 
     app.get('/sse', async (req: Request, res: Response) => {
@@ -28,7 +31,11 @@ export async function connectServer(server: Server): Promise<express.Application
     });
 
     app.listen(port, () => {
-        console.error(`MCP Server running on SSE at http://localhost:${port}`);
+        if(port !== DEFAULT_PORT) {
+            console.error(`Port ${DEFAULT_PORT} is already in use. MCP Server running on SSE at http://localhost:${port}`);
+        } else {
+            console.error(`MCP Server running on SSE at http://localhost:${port}`);
+        }
     });
 
     return app;
